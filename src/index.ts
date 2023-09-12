1c479c450c90c4a8ac6320a9b40dd92c918aaa44
vclemenzi <vclemenzi@users.noreply.github.com>
2023-09-12 17:38:42 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+function useQueryState<T>(name: string, initialState: T | (() => T)): [T, Dispatch<SetStateAction<T>>] {
+  const url = new URL(window.location.href);
+
+  const [val, setVal] = useState(() => {
+    if (typeof initialState == "number") {
+      return parseInt(url.searchParams.get(name) || "") as T || initialState as T;
+    }
+
+    return url.searchParams.get(name) as T || initialState as T;
+  });
+
+  useEffect(() => {
+    url.searchParams.set(name, val as string);
+    window.history.pushState({ path: url.href }, "", url.href);
+  }, [name, val]);
+
+  return [val, setVal];
+}
+
+export { useQueryState };
