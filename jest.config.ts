49a057fb55cc173e1be2bda372ae7c7f96ca97d7
vclemenzi <vclemenzi@users.noreply.github.com>
2023-09-12 18:15:14 +0200

    test: add

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js)$',
+};
+
+export default config;
---
+++ test/state.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { useQueryState } from '../src/index';
+
+function TestComponent() {
+  const [counter, setCounter] = useQueryState("counter", 0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <div>
+      <p>Counter: {counter}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+}
+
+test('does the url update', () => {
+  const { getByText } = render(<TestComponent />);
+  const incrementButton = getByText('Increment');
+
+  fireEvent.click(incrementButton);
+
+  expect(new URL(window.location.href).searchParams.get("counter")).toBe('1');
+});
